76233e96b4752fcf7aae142ad3aee9c47cbe3945
Josue <ijosue.carreon@gmail.com>
2023-03-28 22:11:49 -0500

    backend boilerplate

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+// const client = new Client(process.env.DATABASE_URL);
+const client = new Client('postgres://localhost:5432/gutterBalls');
+
+
+
+
+module.exports = {
+    client
+}
