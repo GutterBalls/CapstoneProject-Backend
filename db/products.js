c0fbf4fcdac27baf1bc5635cf1893be974a22a1b
Bryan Runnels <bryan.d.runnels@gmail.com>
2023-03-30 15:36:31 -0600

    Seed updated with dummy products and category names; createProducts / createCategory functions written in db/products.js - DB tested fine

---
+++ db/products.js
@@ -0,0 +1,39 @@
+const client = require('./client');
+
+// Create a category.
+async function createCategory(name) {
+    try {
+        console.log("starting createCategory");
+        const { rows: [category] } = await client.query(`
+            INSERT INTO product_category(name)
+            VALUES ($1)
+            RETURNING *;
+        `, [name]);
+
+        console.log("finished createCategory");
+        return category;
+
+    } catch (error) {
+        throw error;
+    };
+};
+async function createProducts({image, brand, name, description, price, sale, clearance, category_id}) {
+    try {
+        console.log("staring createProducts");
+        const { rows } = await client.query(`
+            INSERT INTO products(image, brand, name, description, price, sale, clearance, category_id)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            RETURNING *;
+        `, [image, brand, name, description, price, sale, clearance, category_id]);
+
+        console.log("finished createProducts");
+        return rows;
+    } catch (error) {
+        throw error;
+    };
+};
+
+module.exports = {
+    createCategory,
+    createProducts
+}
