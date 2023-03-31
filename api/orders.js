a93bcac03ed5a9d5e9f96ef1247ceda402848bbf
David Burkhart <dburkhart77@gmail.com>
2023-03-31 16:36:49 -0600

    orders api & db

--- api/orders.js
+++ api/orders.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const ordersRouter = express.Router();
 
-const { requireUser } = require('./utils');
+const { requireUser, requireAdmin } = require('./utils');
 
 // Required Functions:
 // getAllOrders
@@ -12,37 +12,103 @@ const { requireUser } = require('./utils');
 // editOrder
 // deleteOrder
 
-//const { 
-    // getAllOrders,
-    // getOrderById,
-    // getOrderByName,
-    // createOrder,
-    // editOrder,
-    // deleteOrder
-// } = require('../db');
+const { 
+    getAllOrders,
+    getOrderById,
+    getOrderByUserId,
+    getOrderByName,
+    createOrder,
+    editOrder,
+    deleteOrder
+} = require('../db');
 
 // Get all orders
-ordersRouter.get('/', async (req, res) => {
-    
+ordersRouter.get('/', requireAdmin, async (req, res, next) => {
+    const allOrders = await getAllOrders();
+
+    res.send(
+        allOrders
+    );
+    next();
 })
 
-// Get order by ID
-ordersRouter.get('/:orderId', async (req, res) => {
-    
+// Get order by ID - User or Admin
+ordersRouter.get('/orderId/:orderId', requireUser || requireAdmin, async (req, res, next) => {
+    const { id } = req.params;
+    const orderById = await getOrderById(id);
+
+    res.send(
+        orderById
+    );
+    next();
 })
 
-// Create new order
+// Get order by userId - User or Admin
+ordersRouter.get('/userId/:userId', requireUser || requireAdmin, async (req, res, next) => {
+    const { id } = req.params;
+    const orderByUserId = await getOrderByUserId(id);
+
+    res.send(
+        orderByUserId
+    );
+    next();
+})
+
+// Create new order - User
 ordersRouter.post('/', requireUser, async (req, res, next) => {
+    try{
+        const { orderDate, orderStatus } = req.body;
 
+        const createdOrder = await createOrder( req.user.id, { orderDate, orderStatus });
+        res.send(
+            createdOrder
+        );
+
+    } catch (error) {
+        throw("Error creating an order", error);
+    };
 })
 
-// update order by ID
-ordersRouter.patch('/:orderId', requireUser, async (req, res, next) => {
+// update order by ID - Admin only
+ordersRouter.patch('/:orderId', requireAdmin, async (req, res, next) => {
+    try{
+        const { id } = req.params;
+        const { orderStatus } = req.body;
 
+        const updatedOrderStatus = await editOrder(id, { orderStatus });
+
+        res.send(
+            updatedOrderStatus
+        );
+
+    } catch (error) {
+        throw("Error updating order status",error);
+    };
 })
 
-// delete order by ID
-ordersRouter.delete('/:orderId', requireUser, async (req, res, next) => {
+// delete order by ID - Admin only
+ordersRouter.delete('/:orderId', requireAdmin, async (req, res, next) => {
+    try{
+        const { id } = req.params;
+        const order = await getOrderById(id);
+        if(order.id){
+            const deletedOrder = await deleteOrder(id);
+                res.send(
+                    deletedOrder
+                );
+        } else {
+            res.send(order ? {
+                name: "UnauthorizedOrderError",
+                message: "You cannot delete an order"
+            } : {
+                name: "OrderNotFoundError",
+                message: "That order does not exist"
+            });
+        };
+
+    } catch (error) {
+        throw("Error deleting order", error);
+    };
 })
 
 module.exports = ordersRouter;
